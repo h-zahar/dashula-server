0eb460cc7bbfbe6d7b57a7233f7a7b76b9dcadfc
h-zahar <hz.rifat@gmail.com>
2023-07-05 12:16:34 +0600

    Connected to MongoDB

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const routes = require("./routes/kpis");
+const KPI = require("./models/kpi");
+const { kpis } = require("./data/data");
 
 dotenv.config();
 const app = express();
@@ -18,15 +21,25 @@ app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
+app.use("/api", routes);
 
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.DB_URL.replace("<user>", process.env.DB_USERNAME)
+      .replace("<pass>", process.env.DB_PASSWORD)
+      .replace("<db>", process.env.DB_NAME),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(async () => {
     app.listen(port, () => {
       console.log(`Server running happily on port: ${port}`);
     });
+
+    /* INSERTING SEED DATA (ONE TIME ONLY) */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis).then(() => console.log("Data inserted!"));
   })
   .catch((err) => console.log(err));
